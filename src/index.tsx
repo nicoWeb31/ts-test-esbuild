b58b5f7c0f97174085ff09c5d68bdf48a75d179f
nicoWeb31 <nico.riot@free.fr>
2021-03-01 20:16:00 +0100

    transpile code ok

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,54 @@
 import ReactDOM from 'react-dom';
+import {useState,useEffect,useRef} from 'react';
+import * as esbuild from 'esbuild-wasm';
+
 
 const App = () =>{
-    return <h1>hi there !!</h1>
+
+
+    const [input,setInput] = useState('');
+    const [code,setCode] = useState('');
+
+    const ref =useRef<any>();
+
+    const startService = async() =>{
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        })
+    }
+
+
+    useEffect(() =>{
+        startService()
+    },[])
+
+    const onClickHandler =async()=>{
+        console.log(input)
+        if(!ref.current){
+            return;
+        }
+
+        const result = await ref.current.transfom(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        })
+
+        setCode(result.code)
+
+    }
+
+    return (
+        <div>
+            <textarea value={input} onChange={e=>setInput(e.target.value)}></textarea>
+            <div>
+                <button type='submit' onClick={onClickHandler}>submit</button>
+            </div>
+            <pre>
+                {code}
+            </pre>
+        </div>
+    )
 }
 
 
