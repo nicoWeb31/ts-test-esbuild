fb447f4c0e2b97ce9cfda821d9f5bf9a40c431f5
nicoWeb31 <nico.riot@free.fr>
2021-03-01 21:21:43 +0100

    transpile code render

--- src/index.tsx
+++ src/index.tsx
@@ -1,55 +1,53 @@
-import ReactDOM from 'react-dom';
-import {useState,useEffect,useRef} from 'react';
-import * as esbuild from 'esbuild-wasm';
+import ReactDOM from "react-dom";
+import { useState, useEffect, useRef } from "react";
+import * as esbuild from "esbuild-wasm";
 
+const App = () => {
+    const [input, setInput] = useState("");
+    const [code, setCode] = useState("");
 
-const App = () =>{
+    const ref = useRef<any>();
 
-
-    const [input,setInput] = useState('');
-    const [code,setCode] = useState('');
-
-    const ref =useRef<any>();
-
-    const startService = async() =>{
+    const startService = async () => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: '/esbuild.wasm'
-        })
-    }
+            wasmURL: "/esbuild.wasm",
+        });
+    };
 
+    useEffect(() => {
+        startService();
+    }, []);
 
-    useEffect(() =>{
-        startService()
-    },[])
-
-    const onClickHandler =async()=>{
-        console.log(input)
-        if(!ref.current){
+    const onClickHandler =  async() => {
+        if (!ref.current) {
             return;
         }
 
-        const result = await ref.current.transfom(input, {
+        // console.log(ref.current)
+        const result = await ref.current.transform(input,{
             loader: 'jsx',
             target: 'es2015'
         })
+        console.log("🚀 ~ file: index.tsx ~ line 32 ~ onClickHandler ~ result", result)
 
-        setCode(result.code)
-
-    }
+        setCode(result.code);
+    };
 
     return (
         <div>
-            <textarea value={input} onChange={e=>setInput(e.target.value)}></textarea>
+            <textarea
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            ></textarea>
             <div>
-                <button type='submit' onClick={onClickHandler}>submit</button>
+                <button type="submit" onClick={onClickHandler}>
+                    submit
+                </button>
             </div>
-            <pre>
-                {code}
-            </pre>
+            <pre>{code}</pre>
         </div>
-    )
-}
+    );
+};
 
-
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
