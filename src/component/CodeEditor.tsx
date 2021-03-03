14a1898df9692e4cbf3d6aef91107a0ee550cc59
nicoWeb31 <nico.riot@free.fr>
2021-03-03 12:35:34 +0100

    monaco editor config

--- src/component/CodeEditor.tsx
+++ src/component/CodeEditor.tsx
@@ -3,7 +3,7 @@ import MonacoEditor from '@monaco-editor/react';
 import './code-editor.style.css'
 
 const CodeEditor = () => {
-    return <MonacoEditor className="_editor"/>
+    return <MonacoEditor className="_editor" language="javascript" theme="dark" options={{wordwrap: 'on'}}/>
 }
 
 
