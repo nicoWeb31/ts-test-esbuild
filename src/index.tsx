bcd391b40c61d0bdd343a32fb25b17fde575dc05
nicoWeb31 <nico.riot@free.fr>
2021-03-04 12:12:08 +0100

    refacto suite

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import ReactDOM from "react-dom";
-import { useState, useEffect, useRef } from "react";
-import * as esbuild from "esbuild-wasm";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-pluging";
-import { fetchPlugin } from "./plugins/fecth-plugin";
+import { useState } from "react";
+import { bundler } from './bundler/index'
+
 import CodeEditor from "./component/CodeEditor";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import Preview from "./component/Preview";
@@ -12,36 +11,11 @@ const App = () => {
     const [input, setInput] = useState("");
     const [code, setCode] = useState("");
 
-    const ref = useRef<any>();
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-        });
-    };
-
-    useEffect(() => {
-        startService();
-    }, []);
 
     const onClickHandler = async () => {
-        if (!ref.current) {
-            return;
-        }
-
-        const result = await ref.current.build({
-            entryPoints: ["index.js"],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-            define: {
-                "process.env.NODE_ENV": '"production"',
-                global: "window",
-            },
-        });
-
-        setCode(result.outputFiles[0].text);
+        const output = await bundler(input)
+        setCode(output);
     };
 
     return (
