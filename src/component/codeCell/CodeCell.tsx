c9c5fe9f630e34ad71d7378ad45cf7af8706c55b
nicoWeb31 <nico.riot@free.fr>
2021-03-10 17:58:23 +0100

    progress bar css

--- src/component/codeCell/CodeCell.tsx
+++ src/component/codeCell/CodeCell.tsx
@@ -5,7 +5,7 @@ import Resizable from "../Resizable";
 import { useAction } from "../../hooks/useAction";
 import { Cell } from "../../state/cell";
 import { useTypedSelector } from "../../hooks/use-typed-selector";
-import './codeCell.style.css'
+import "./codeCell.style.css";
 
 interface CodeCellProps {
     cell: Cell;
@@ -45,14 +45,20 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
                         onChange={(value: string) => updateCell(cell.id, value)}
                     />
                 </Resizable>
-
-                {!bundle || bundle.loading ? (
-                    <div className="progress-cover">
-                        <progress className ="progress is-smal is-primary" max="100">Loading...</progress>
-                    </div>
-                ) : (
-                    <Preview code={bundle.code} err={bundle.err} />
-                )}
+                <div className="progress-wrapper">
+                    {!bundle || bundle.loading ? (
+                        <div className="progress-cover">
+                            <progress
+                                className="progress is-smal is-primary"
+                                max="100"
+                            >
+                                Loading...
+                            </progress>
+                        </div>
+                    ) : (
+                        <Preview code={bundle.code} err={bundle.err} />
+                    )}
+                </div>
             </div>
         </Resizable>
     );
