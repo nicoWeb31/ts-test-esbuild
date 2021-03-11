e2728cc173cc3224c2f79723a46f5cfc467da3fb
nicoWeb31 <nico.riot@free.fr>
2021-03-11 11:12:58 +0100

    show fuction complex value

--- src/component/codeCell/CodeCell.tsx
+++ src/component/codeCell/CodeCell.tsx
@@ -21,11 +21,29 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
         const cumuCode: any = [
 
+
             `
+
+            import _React from 'react';
+            import _ReactDOM from 'react-dom';
+            const root = document.querySelector('#root');
             const show = (value) =>{
-                document.querySelector('#root').innerHTML = value;
+                if(typeof value === 'object'){
+                    if(value.$$typeof && value.props){
+                        _ReactDOM.render(value,root)
+                        
+
+                    }else{
+                        root.innerHTML = JSON.stringify(value);
+
+                    }
+
+                }else{
+                    root.innerHTML = value;
+
+                }
             }
-            `
+            `,
         ];
         for (let c of orderedCells) {
             if (c.type === "code") {
